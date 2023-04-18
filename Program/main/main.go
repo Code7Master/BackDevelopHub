7cbd97666ba80434228f9c447675178918a7b38d
JunBeomHan <2hanjunbum6@gmail.com>
2023-04-19 00:44:50 +0900

    Add logger command

--- Program/main/main.go
+++ Program/main/main.go
@@ -8,5 +8,7 @@ import (
 const PORT = ":9190"
 
 func main() {
+	// 아래의 코드가 먼저 실행되게 하기
+	// 	log.NewLogger().Record("[DevelopHub] Server Start!!")
 	http.ListenAndServe(PORT, myapp.NewHTTPHandler())
 }
