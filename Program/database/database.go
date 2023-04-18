5683842ead7f4dc6f438bdf9bd256fabaf0c8bd0
JunBeomHan <2hanjunbum6@gmail.com>
2023-04-19 00:45:34 +0900

    Add logger command

--- Program/database/database.go
+++ Program/database/database.go
@@ -1,9 +1,9 @@
 package database
 
 import (
+	"BackDevelopHub/Program/log"
 	"database/sql"
 	"fmt"
-	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,45 +16,49 @@ const (
 	DBNAME   = "DevelopHub"
 )
 
-var DBInfo string = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", USER, PASSWORD, HOST, PORT, DBNAME)
+var logger log.Logger = *log.NewLogger()
+var DBEnvironment string = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", USER, PASSWORD, HOST, PORT, DBNAME)
 
-type DatabaseQuery struct {
+type DatabaseQuerier struct {
 }
 
 // table structs
 var user User = User{}
 
 func InitDB() *sql.DB {
-	db, err := sql.Open("mysql", DBInfo)
+	logger.Record("[BackDevelopHub/Program/database/database.go (func InitDB)__GOOD] start")
+	db, err := sql.Open("mysql", DBEnvironment)
 	if err != nil {
-		fmt.Println("[Database ERROR]: ", err)
+		logger.Record(fmt.Sprintf("[BackDevelopHub/Program/database/database.go (func InitDB)__ERROR] Do not opne mysql: %s", err))
 	}
 	return db
 }
 
-func NewDatabaseQuery() *DatabaseQuery {
-	return &DatabaseQuery{}
+func NewDatabaseQuerier() *DatabaseQuerier {
+	return &DatabaseQuerier{}
 }
 
 // SELECT COUNT(*) FROM user_table WHERE username='JunBeomHan' OR email='1234'"
-func (d *DatabaseQuery) IsSameUsernameEmail(db *sql.DB, username, email string) bool {
+func (d *DatabaseQuerier) IsSameUsernameOREmail(db *sql.DB, username, email string) bool {
+	logger.Record("[BackDevelopHub/Program/database/database.go (func IsSameUsernameOREmail)__GOOD] start")
 	var count int
 	query := fmt.Sprintf(`SELECT COUNT(*) FROM user_table WHERE username='%s' OR email='%s'`, username, email)
 	res := db.QueryRow(query)
 	err := res.Scan(&count)
 	if err != nil {
-		fmt.Println("[Database IsSameUsernameEmail ERROR]: ", err)
+		logger.Record(fmt.Sprintf("[BackDevelopHub/Program/database/database.go (func IsSameUsernameOREmail)__ERROR] Do not read to sql query: %s", err))
 		return false
 	}
 	return count > 0
 }
 
 // INSERT INTO user_table(username, email, passwrod) VALUES('JunBeomHan', 'agc@gmail.com', '123132')
-func (d *DatabaseQuery) SingUp(db *sql.DB, username, email, password string) {
+func (d *DatabaseQuerier) SingUp(db *sql.DB, username, email, password string) {
+	logger.Record("[BackDevelopHub/Program/database/database.go (func SingUp)__GOOD] start")
 	query := fmt.Sprintf(`INSERT INTO user_table(username, email, password) VALUES('%s', '%s', '%s')`, username, email, password)
 	_, err := db.Exec(query)
 
 	if err != nil {
-		log.Fatal(err)
+		logger.Record(fmt.Sprintf("[BackDevelopHub/Program/database/database.go (func SingUp)__ERROR] Do not insert to mysql: %s", err))
 	}
 }
