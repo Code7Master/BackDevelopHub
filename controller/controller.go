41bb0083ccd33be69b5ce1bba41b5a9c5a4a674d
JunBeomHan <2hanjunbum6@gmail.com>
2023-05-02 21:13:20 +0900

    Add controller

---
+++ controller/controller.go
@@ -0,0 +1,26 @@
+package controller
+
+import ( 
+	"BackDevelopHub/service/handler"	
+	"net/http"
+
+)
+func NewHTTPHandler() http.Handler {
+	
+	mux := http.NewServeMux()
+
+	// user
+	mux.HandleFunc("/auth/singup", handler.AuthSingup)
+	mux.HandleFunc("/auth/login", handler.AuthLogin)
+	
+	// question
+	mux.HandleFunc("/question/totla", handler.QuestionTotal)
+	mux.HandleFunc("/question/preview", handler.QuestionPreview)	// query string
+	mux.HandleFunc("/question/detail", handler.QuestionDetail)		// query string
+	
+	//answer
+
+
+
+	return mux
+}
