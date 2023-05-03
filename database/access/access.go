e989900940a4adbae146082dd8a0f76b6bb84b22
JunBeomHan <2hanjunbum6@gmail.com>
2023-05-03 11:41:00 +0900

    GetQuestionPreviewByIndex

--- database/access/access.go
+++ database/access/access.go
@@ -1,7 +1,9 @@
 package access
 
 import (
+	"BackDevelopHub/dhjson"
 	"fmt"
+	
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -17,9 +19,22 @@ func init() {
 }
 
 func GetRowsCount() int {
+	query := "SELECT COUNT(*) FROM question_table"
+
 	var rowsCount int
-	if err := db.QueryRow("SELECT COUNT(*) FROM question_table").Scan(&rowsCount); err != nil {
-		fmt.Println("db.QueryRow error: ", err)
+	if err := db.QueryRow(query).Scan(&rowsCount); err != nil {
+		fmt.Println("QueryRow error: ", err)
 	}
 	return rowsCount
+}
+
+func GetQuestionPreviewByIndex(question_idx int) dhjson.QuestionPreview {
+	query := fmt.Sprintf("SELECT * FROM question_table WHERE idx=%d", question_idx)
+	
+	var questionPreview dhjson.QuestionPreview
+	var ignore interface{}
+	if err := db.QueryRow(query).Scan(&ignore, &questionPreview.Writer, &questionPreview.Title, &ignore, &questionPreview.View_count, &questionPreview.Answer_count, &questionPreview.Vote_count); err != nil {
+		fmt.Println("GetQuestionPreviewByIndex error: ", err)
+	}
+	return questionPreview
 }
