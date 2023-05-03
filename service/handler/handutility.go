1ff0d4b3939791fef2ecfdb5b9590b05311b58a7
JunBeomHan <2hanjunbum6@gmail.com>
2023-05-03 11:39:59 +0900

    Add atoi func

--- service/handler/handutility.go
+++ service/handler/handutility.go
@@ -5,9 +5,21 @@ import (
 	"net/http"
 )
 
-func sendJSONResponse(responseWriter http.ResponseWriter, statusCode int, json string) {
-	fmt.Println("sendJSONResponse")
+func sendJSONResponse(responseWriter http.ResponseWriter, statusCode int, json []byte) {
 	responseWriter.Header().Set("Content-Type", "application/json");
 	responseWriter.WriteHeader(statusCode)
-	responseWriter.Write([]byte(json))
+	responseWriter.Write(json)
+}
+
+func atoi(str string) int {
+	var resVal int
+	strLen := len(str)
+
+	for i := 0; i < strLen; i++ {
+		resVal += int(str[i]) - '0'
+		if i+1 != strLen {
+			resVal *= * 10
+		}
+	}
+	return resVal
 }
